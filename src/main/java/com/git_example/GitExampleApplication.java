07d0974429134a9391fb04e3e62e415683e5a316
israr2025 <114759477+israr2025@users.noreply.github.com>
2026-05-09 02:30:41 +0530

    Update GitExampleApplication.java
    Second commite

--- src/main/java/com/git_example/GitExampleApplication.java
+++ src/main/java/com/git_example/GitExampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitExampleApplication {
     int a=10;
+	int b=100;
 	public static void main(String[] args) {
 		SpringApplication.run(GitExampleApplication.class, args);
 	}

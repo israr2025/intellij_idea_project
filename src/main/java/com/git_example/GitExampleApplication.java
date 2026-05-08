be07094ba02fcec5cd27cb91d711795a67c7e5f7
israr2025 <mrisrar752@gmail.com>
2026-05-09 02:35:53 +0530

    Second commit

--- src/main/java/com/git_example/GitExampleApplication.java
+++ src/main/java/com/git_example/GitExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitExampleApplication {
     int a=10;
 	int b=100;
+    int c=1000;
 	public static void main(String[] args) {
 		SpringApplication.run(GitExampleApplication.class, args);
 	}
